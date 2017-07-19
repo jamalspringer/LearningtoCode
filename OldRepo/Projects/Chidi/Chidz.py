ef765dc7e2ae93a6d95c718241a6ebf229f3b1ff
jspringer <jamal.springer@outlook.com>
2017-07-19 23:00:57 +0100

    Added Old repo into github repo

---
+++ OldRepo/Projects/Chidi/Chidz.py
@@ -0,0 +1,16 @@
+
+Name = raw_input("What is your full name ? ")
+Age = raw_input("How old are you ? ")
+Gender = raw_input("Are you Male or Female? ")
+Height = raw_input("Please provide your height in CM ")
+Weight_InStones= raw_input("How much do you weigh ? ")
+Smoker= raw_input("Do you smoke ? ")
+
+Occupation= raw_input("What do you do for a living ? ")
+Education_Level = raw_input("What is your highest level of education ? ")
+
+Address  =  raw_input(" What is the first line of your address ? ")
+Postcode = raw_input("What is your post code ? ")
+
+print "So your name is %s and you are %s" % (Name, Age)
+
---
+++ OldRepo/Projects/Chidi/Questions.py
@@ -0,0 +1,9 @@
+
+
+
+
+Name = raw_input("What is your name ? ")
+Age = raw_input("How old are you ? ")
+Race = raw_input("What ethnicity are you ? ")
+
+print "Thank you for entering your details"
