bd274ef20437783a902cf2b9cc7b750e9859eab0
Jamal Springer <jamal.springer@outlook.com>
2017-11-25 23:12:32 +0000

    Updated basic.rb, it's now a basic user name creation program.

--- Ruby/basic.rb
+++ Ruby/basic.rb
@@ -1,1 +1,14 @@
+#Simple program to take a user's first and last name plus their year of birth and generate a userID.
+
+puts "First Name:"
+first_name = gets.chomp
+puts "Last Name:"
+last_name = gets.chomp
+puts "Year of birth:"
+yob = gets.chomp
+
+user_id = first_name + last_name + yob.to_s
+puts "Your User ID is  #{user_id}"
+
+
 
