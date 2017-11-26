a8332069db128941ffe14a1f3eb1219300e61e70
Jamal Springer <jamal.springer@outlook.com>
2017-11-26 00:52:19 +0000

    Updated basics.rb, code utilizing Ruby hashing / dictionaries has been added

--- Ruby/basic.rb
+++ Ruby/basic.rb
@@ -10,5 +10,13 @@ yob = gets.chomp
 user_id = first_name + last_name + yob.to_s
 puts "Your User ID is  #{user_id}"
 
+family = {
+    "Deonn" => 29,
+    "Jamal" => 24,
+    "Javon" => 20,
+    "Shemaiah" => 13
+}
 
-
+family.each do |name, age|
+  puts "#{name} is currently #{age} years old"
+end
