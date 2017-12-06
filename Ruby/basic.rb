af095556a7c656a34fdff147282d3adec4f42c75
Jamal Springer <jamal.springer@outlook.com>
2017-12-06 22:18:38 +0000

    Updated the userID creation step to create username as lower case string, rather than a mixture of upper & lower case.

--- Ruby/basic.rb
+++ Ruby/basic.rb
@@ -7,7 +7,7 @@ last_name = gets.chomp
 puts "Year of birth:"
 yob = gets.chomp
 
-user_id = first_name + last_name + yob.to_s
+user_id = first_name[0].downcase + last_name.downcase + yob.to_s
 puts "Your User ID is  #{user_id}"
 
 family = {
