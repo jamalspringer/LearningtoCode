1d8bb3c1f21c6817519d64417bbf46c1a0e5d606
Jamal Springer <jamal.springer@outlook.com>
2017-12-20 23:49:54 +0000

    Deleted old/unused Python notebook files and added a new human class file

---
+++ Ruby/Ruby_with_mastery_learning/human_class.rb
@@ -0,0 +1,41 @@
+
+class Human
+  def initialize(name, age, sex, nationality, race)
+    @name = name
+    @age = age
+    @sex = sex
+    @nationality = nationality
+    @race = race
+  end
+
+
+  public
+
+  def greet(person)
+    puts "Hello #{person}, How are you ?"
+  end
+
+  def introduce(person)
+    puts "Hello #{person}, my name is #{@name},i'm #{@age} years old & i'm #{@nationality}"
+  end
+
+  def conversate(words)
+    puts words
+  end
+
+  def study(subject, hours=1)
+    if hours == 1
+      puts "Currently studying #{subject} for an hour"
+    else
+      puts "Currently studying #{subject} for #{hours} hours"
+    end
+  end
+
+end
+
+jspringer = Human.new("Jamal Springer", 24, "Male", "British", "Black")
+
+jspringer.greet("Steve")
+jspringer.introduce("Bobby")
+jspringer.conversate("Blah Blah Blah, Yada Yada Yada & yeah my weekend was amazing!")
+jspringer.study("Coding Ruby", 3)
